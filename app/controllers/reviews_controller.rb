054d542d182cdd0adea3263693595a5d460ff3ce
Guilherme Esteves <guiesteves1985@gmail.com>
2019-09-27 16:09:00 -0300

    implementing review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,18 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant
+
+def new
+  @review = Review.new
+end
+
+def create
+end
+
+private
+
+def set_restaurant
+  @restaurant = Restaurant.find(params[:restaurant_id])
+end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      get 'top', to: 'restaurants#top'
+    end
+    member do                           # member => restaurant id in URL
+      get 'chef', to: 'restaurants#chef'
+    end
+    resources :reviews, only: [:new, :create]
+
+  end
 end
